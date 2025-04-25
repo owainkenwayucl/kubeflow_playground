7bacac4b6329814c00280c816ab671188e7245b5
Owain Kenway <o.kenway@ucl.ac.uk>
2025-04-25 11:59:48 +0100

    Ah! this is the generic endpoint

--- learning_pipelines/hello.py
+++ learning_pipelines/hello.py
@@ -14,7 +14,7 @@ def hello_pipeline(recipient: str) -> str:
 
 compiler.Compiler().compile(hello_pipeline, 'hello_pipeline.yaml')
 
-client = Client(host='http://10.43.246.237:80')
+client = Client(host='ml-pipeline.kubeflow.svc.cluster.local:8888')
 run = client.create_run_from_pipeline_package(
     'hello_pipeline.yaml',
     arguments={
