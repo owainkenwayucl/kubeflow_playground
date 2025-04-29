b7ba50096bf85422e382b53f344b8f718950b8e6
Owain Kenway <o.kenway@ucl.ac.uk>
2025-04-29 16:59:39 +0100

    Testing pytorch

---
+++ learning_pipelines/testpytorch.py
@@ -0,0 +1,25 @@
+from kfp import dsl, compiler
+from kfp.client import Client
+
+@dsl.component(base_image="nvcr.io/nvidia/pytorch:25.03-py3")
+def count_gpus() -> str:
+    import torch
+    num_gpus = torch.cuda.device_count()
+    r = f"{bold_on}Detected {torch.cuda.device_count()} Cuda devices.{style_off}"
+    for a in range(torch.cuda.device_count()):
+        r = r + f"{bold_on}Detected Cuda Device {a}:{style_off} {torch.cuda.get_device_name(a)}"
+    print(r)
+    return r
+
+@dsl.pipeline
+def gpu_pipeline() -> str:
+    gpu_task = count_gpus().set_gpu_limit(1)
+    return gpu_task.output
+
+compiler.Compiler().compile(gpu_pipeline, 'gpu_pipeline.yaml')
+
+client = Client()
+run = client.create_run_from_pipeline_package(
+    'gpu_pipeline.yaml',
+    arguments={},
+)
